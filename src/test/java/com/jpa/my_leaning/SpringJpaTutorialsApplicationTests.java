b2d5c858f9eb29ff08e6e452154e86231a3694e1
LightningShip <dev@lightningshipstore.com>
2022-08-24 08:39:03 +0200

    Tutorial Init

---
+++ src/test/java/com/jpa/my_leaning/SpringJpaTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.my_leaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
